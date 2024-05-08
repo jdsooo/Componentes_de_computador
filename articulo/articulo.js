2b8db815efa56f0e3c31aec56cf48146a112f502
forellanabarrios@gmail.com <forellanabarrios@gmail.com>
2024-05-08 14:35:31 -0300

    version 4

--- articulo/articulo.js
+++ articulo/articulo.js
@@ -1,1 +1,6 @@
-console
+let fkm=document.querySelector(".articulo");
+ fkm.innerHTML=`
+ <h1>gpu</h1>
+ <p>
+    La GPU, o Unidad de Procesamiento Gráfico, es un componente esencial en sistemas informáticos modernos que se especializa en el procesamiento y la generación de gráficos. Aquí tienes información más detallada sobre la GPU:</p>
+ `;
--- sub menu/sub menu.js
+++ sub menu/sub menu.js
@@ -1,1 +1,18 @@
-let submenu=document.querySelector("secccion2") 
+let submenu=document.querySelector(".menu");
+
+submenu.innerHTML=`
+<ul class="nav">
+    <li>cpu</li>
+    <br>
+    <li>gpu</li>
+    <br>
+    <li>disco duro</li>
+    <br>
+    <li>unidad ssd</li>
+    <br>
+    <li>ram</li> 
+    <br>
+    <li>tarjeta madre</li>
+</ul>
+`;
+ 
