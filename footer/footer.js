674013f1755c368d624f2060be2a7e144bb4ed2b
jdsooo <jdsaquiqu@gmail.com>
2024-05-09 10:33:46 -0600

    version jdso1

--- footer/footer.js
+++ footer/footer.js
@@ -1,1 +1,9 @@
-console
+let li_jsaquique=document.querySelector(".footer")
+    li_jsaquique.innerHTML=`
+    <div>
+    <img src=" https://static.wixstatic.com/media/d1b317_cae84267083b4acda55397885149c735~mv2.png" alt="">
+    </div>
+    <div class="lo">
+    <p>Redes Sociales</p>
+    </div>
+    `;
--- header/header.js
+++ header/header.js
@@ -2,11 +2,8 @@ let li_jsaquique=document.querySelector(".header");
  
 li_jsaquique.innerHTML=`
 <div>
-
-<img src="https://raw.githubusercontent.com/jdocdev/NavbarsFlexbox/main/img/Mountain.png" alt="Logo de marca">
+<img src="https://static.wixstatic.com/media/d1b317_cae84267083b4acda55397885149c735~mv2.png" alt="Logo de marca">
 </div>
-
-
 <div class="le">
 <p>home</p>
 <p>contact</p>
