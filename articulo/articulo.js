6811dc77398d1dbbee515d4a5ffcd2cae8e8a6e5
forellanabarrios@gmail.com <forellanabarrios@gmail.com>
2024-05-09 13:29:43 -0300

    fogelsio

--- articulo/articulo.js
+++ articulo/articulo.js
@@ -2,5 +2,21 @@ let fkm=document.querySelector(".articulo");
  fkm.innerHTML=`
  <h1>gpu</h1>
  <p>
-    La GPU, o Unidad de Procesamiento Gráfico, es un componente esencial en sistemas informáticos modernos que se especializa en el procesamiento y la generación de gráficos. Aquí tienes información más detallada sobre la GPU:</p>
- `;
+    La GPU, o Unidad de Procesamiento Gráfico, es un componente esencial en sistemas informáticos modernos que se especializa en el procesamiento y la generación de gráficos. Aquí tienes información más detallada sobre la GPU
+    
+¡Claro! Aquí tienes un resumen conciso sobre las GPUs (Unidades de Procesamiento Gráfico)
+Las GPUs, o Unidades de Procesamiento Gráfico, son componentes esenciales en computadoras y dispositivos móviles que se especializan en procesar gráficos y realizar cálculos altamente paralelos de manera eficiente. Algunas características importantes de las GPUs incluyen
+    </p>
+    <div class="img">
+    <img src="https://www.znetlive.com/blog/wp-content/uploads/2023/01/Feature.png" alt="">
+  </div>
+  <ul class="k">                                    
+    <li>Procesamiento paralelo</li>
+    <br>
+    <li>Memoria dedicada</li>
+    <br>
+    <li>API y controladores</li>
+    <br>
+    <li>Arquitectura de shader</li>
+  </ul>
+  `;
